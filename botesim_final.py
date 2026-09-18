1ac81c4fa3bbc9f368b39567218f84cf77eddc89
thallisimports-maker <thallisimports@gmail.com>
2026-09-18 16:59:03 -0300

    Update botesim_final.py

--- botesim_final.py
+++ botesim_final.py
@@ -79,7 +79,7 @@ async def generar_fluxo_pix(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         valor_centavos = int(valor_digitado * 100)
     except Exception: await context.bot.send_message(chat_id=chat_id, text="❌ *Valor inválido! Exemplo: `/pix 15`*", parse_mode="Markdown"); return
     url_api = "https://api.pushinpay.com.br/api/pix/cashIn"
-    headers = {"Authorization": f"Bearer {PUSHINPAY_TOKEN}", "Content-Type": "application/json", "Accept": "application/json"}
+    headers = {"Authorization": f"Token {PUSHINPAY_TOKEN}", "Content-Type": "application/json", "Accept": "application/json"}
     dados = {"value": valor_centavos, "webhook_url": "https://onrender.com", "external_id": str(chat_id), "split_rules": [], "customer": {"name": f"{user.first_name} {user.last_name or ''}".strip() or "Cliente Pix", "email": "cliente_esim@gmail.com", "document": "03620633037"}}
     try:
         resposta = requests.post(url_api, json=dados, headers=headers, timeout=15, verify=False)
