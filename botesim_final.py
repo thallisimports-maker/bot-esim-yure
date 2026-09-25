3e0bcf30ac0ef57c07ff2f604d46f229fbc398c1
thallisimports-maker <thallisimports@gmail.com>
2026-09-25 13:56:57 -0300

    Update botesim_final.py

--- botesim_final.py
+++ botesim_final.py
@@ -1322,8 +1322,7 @@ async def gerar_pix_miniapp(payload: PayloadRecargaMiniApp):
 
         body = {
             "value": int(round(valor * 100)),  # Valor em centavos
-            "webhook_url": f"{URL_BACKEND}/webhook/pushinpay",
-        }
+            "webhook_url": f"{URL_BACKEND}/webhook/pushinpay?user_id={user_id}",
 
         async with httpx.AsyncClient() as client:
             resp = await client.post(
